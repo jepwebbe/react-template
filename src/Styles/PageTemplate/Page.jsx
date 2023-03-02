b283141c12f668f0cfcb95eb7325002407fff768
jepwebbe <jepwebbe@gmail.com>
2023-03-02 20:53:06 +0100

    spelling error

--- src/Styles/PageTemplate/Page.jsx
+++ src/Styles/PageTemplate/Page.jsx
@@ -4,7 +4,7 @@ import { StyledGrid } from "./Page.Styled";
 const Page = (props) => {
   // Destructuring props object to extract the children, title, description, and subtitle properties
   const { children, title, description } = props;
-  // Using useEffect  to update the document title and description whenever they change
+  // Using useEffect to update the document title and description whenever they change
   useEffect(() => {
     document.title = title;
     // If a description it exists, it sets the content attribute of the description meta tag to the value of the "description" prop
